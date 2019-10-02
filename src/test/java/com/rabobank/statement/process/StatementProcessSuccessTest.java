558170d359d400e01be692d3d7fe38982adc7e32
Kingshuk <mukherjee.kingshuk@gmail.com>
2019-10-03 01:49:10 +0530

    Fine tuning - updated

---
+++ src/test/java/com/rabobank/statement/process/StatementProcessSuccessTest.java
@@ -0,0 +1,88 @@
+package com.rabobank.statement.process;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.test.AssertFile;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.MetaDataInstanceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <h1>Statement process Test success!</h1>
+ *
+ * @author Kingshuk Mukherjee - 233303
+ * @version 1.0
+ * @since 02/10/2019
+ */
+
+@ContextConfiguration(classes = {CustomerStatementProcessConfiguration.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class StatementProcessSuccessTest {
+
+    private final int EXPECTED_COUNT = 3;
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+    @Autowired
+    private FlatFileItemReader<CustomerStatementModel> reader;
+    @Autowired
+    private FlatFileItemWriter<CustomerStatementModel> writer;
+
+    @Value("classpath:/input/test_positive_records.csv")
+    private Resource testInputResourceSuccess;
+
+    private Resource testOutputResourceSuccess =
+            new FileSystemResource("src/test/resources/output/reconciliationReportSuccess.csv");
+
+
+    @Before
+    public void setUp() throws Exception {
+        reader.setResource(testInputResourceSuccess);
+        reader.setLinesToSkip(1);
+
+        // open, provide "mock" ExecutionContext
+        reader.open(MetaDataInstanceFactory.createStepExecution().getExecutionContext());
+
+        //Set output file location
+        writer.setResource(testOutputResourceSuccess);
+        writer.setAppendAllowed(false);
+        writer.setShouldDeleteIfExists(true);
+        writer.open(MetaDataInstanceFactory.createStepExecution().getExecutionContext());
+    }
+
+    @Test
+    public void testRecordsCount() throws Exception {
+
+        // read
+        int count = 0;
+        while (reader.read() != null) {
+            count++;
+        }
+        assertEquals(EXPECTED_COUNT, count);
+
+    }
+
+    @Test
+    public void testWriter() throws Exception {
+        // no reports will be generated in case of success
+        AssertFile.assertLineCount(0, testOutputResourceSuccess);
+    }
+
+    @Test
+    public void testCsvFileProcessingJobSuccess() throws Exception {
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobLauncherTestUtils.getUniqueJobParameters());
+        Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+    }
+}
